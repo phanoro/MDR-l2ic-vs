af009d8c096bb46db6c1763c8a2b93c2882d2bdd
JavaVivi <34030149+JavaVivi@users.noreply.github.com>
2017-12-28 14:12:05 +0100

    Update SiteMapComponents.java

--- SiteMapComponents.java
+++ SiteMapComponents.java
@@ -10,42 +10,70 @@ public class SiteMapComponents {
 
     public SiteMapComponents() {
 		// TODO Auto-generated constructor stub
-
+/**
+ * @author Virginie/Stéphane
+ * @version 1.0
+ */
         this.freqChg="monthly";
         this.prio="0.5";
     }
-
+/**
+ * 
+ * @return L'url de la page
+ */
     public String getUrl() {
     	String lien = this.url;
     	int debut = lien.lastIndexOf("/");
     	String texte = lien.substring(debut + 1);
         return texte;
     }
+    /**
+     * 
+     * @param url
+     */
 
     public void setUrl(String url) {
         this.url = url;
     }
-
+/**
+ * 
+ * @return La date de la page.
+ */
     public String getDate() {
         return date;
     }
-
+/**
+ * 
+ * @param date
+ */
     public void setDate(String date) {
         this.date = date;
     }
-
+/**
+ * 
+ * @return La priorité de la page.
+ */
     public String getPrio() {
         return prio;
     }
-
+/**
+ * 
+ * @param prio
+ */
     public void setPrio(String prio) {
         this.prio = prio;
     }
-
+/**
+ * 
+ * @return La fréquence des mises à jour de la page
+ */
     public String getFreqChg() {
         return freqChg;
     }
-
+/**
+ * 
+ * @param freqChg
+ */
     public void setFreqChg(String freqChg) {
         this.freqChg = freqChg;
     }
