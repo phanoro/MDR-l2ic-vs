dc3a7f859e228475653b3044f51532226ccb5d36
JavaVivi <34030149+JavaVivi@users.noreply.github.com>
2017-12-28 16:20:28 +0100

    Update SiteMapCreatorLauncher.java

--- SiteMapCreatorLauncher.java
+++ SiteMapCreatorLauncher.java
@@ -2,13 +2,28 @@ package gui;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import javax.swing.JFrame;
+import java.util.Scanner;
 
-public class SiteMapCreatorLauncher {										//The programme's launching class
+import javax.swing.JFrame;
+/**
+ * 
+ * @author Virginie/Stéphane
+ * @version 1.0
+ *
+ */
+public class SiteMapCreatorLauncher {										
 	public static void main(String[]args){
 		init("SiteMap");
 	}
 	
+	Scanner sc = new Scanner(System.in);
+    System.out.println(" Please type your keyword:");
+    int str = sc.nextInt();
+    System.out.println("You choose the following keyword : " + str);
+    /**
+     * 
+     * @param name
+     */
 	public static void init(String name){
 		
 		JFrame frame = new JFrame(name);
